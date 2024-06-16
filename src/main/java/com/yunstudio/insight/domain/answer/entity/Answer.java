4e13baee480e207bc80f25b60e6fcd71bd1a32bd
barami62 <wodbs7758@naver.com>
2024-06-16 18:57:28 +0900

    Feat: Answer 엔티티 추가

---
+++ src/main/java/com/yunstudio/insight/domain/answer/entity/Answer.java
@@ -0,0 +1,49 @@
+package com.yunstudio.insight.domain.answer.entity;
+
+import com.yunstudio.insight.domain.model.SoftDeleteEntity;
+import com.yunstudio.insight.domain.question.entity.Question;
+import com.yunstudio.insight.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "answers")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Answer extends SoftDeleteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(name = "content", nullable = false, length = 65535)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @Builder
+    private Answer(String content, User author, Question question) {
+        this.content = content;
+        this.author = author;
+        this.question = question;
+    }
+}
