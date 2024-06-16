24ebe8536cffcc01a060566493b5e86cab618842
barami62 <wodbs7758@naver.com>
2024-06-16 17:55:13 +0900

    first commit

---
+++ src/main/java/com/yunstudio/insight/InsightApplication.java
@@ -0,0 +1,12 @@
+package com.yunstudio.insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightApplication.class, args);
+    }
+}
