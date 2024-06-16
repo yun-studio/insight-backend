3b0d04e63feca76683a5221c6839fc029b56c1a5
barami62 <wodbs7758@naver.com>
2024-06-16 18:57:35 +0900

    Feat: Like 엔티티 추가

---
+++ src/main/java/com/yunstudio/insight/domain/like/entity/Like.java
@@ -0,0 +1,42 @@
+package com.yunstudio.insight.domain.like.entity;
+
+import com.yunstudio.insight.domain.answer.entity.Answer;
+import com.yunstudio.insight.domain.model.BaseEntity;
+import com.yunstudio.insight.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "likes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    @Builder
+    private Like(User user, Answer answer) {
+        this.user = user;
+        this.answer = answer;
+    }
+}
