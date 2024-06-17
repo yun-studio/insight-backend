c3ea832bd7d5d5e9fbf3e8990ed78c5e04d32413
barami62 <wodbs7758@naver.com>
2024-06-17 16:46:07 +0900

    Feat: OAuth2 시큐리티 설정 추가

--- src/main/java/com/yunstudio/insight/global/security/WebSecurityConfig.java
+++ src/main/java/com/yunstudio/insight/global/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yunstudio.insight.global.security;
 
+import com.yunstudio.insight.domain.user.service.GoogleOAuth2UserService;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +25,9 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final GoogleOAuth2UserService googleOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -39,10 +44,15 @@ public class WebSecurityConfig {
 
         // CSRF 설정
         http.csrf(AbstractHttpConfigurer::disable);
+
         // CORS 설정
         http.cors(getCorsConfigurerCustomizer());
+
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // OAuth2 Login 설정
+        http.oauth2Login(getoAuth2LoginConfigurerCustomizer());
 
         // 요청 URL 접근 설정
         settingRequestAuthorization(http);
@@ -65,6 +75,16 @@ public class WebSecurityConfig {
     }
 
     /**
+     * OAuth2 Login 설정
+     */
+    private Customizer<OAuth2LoginConfigurer<HttpSecurity>> getoAuth2LoginConfigurerCustomizer() {
+        return oAuth2LoginConfig -> oAuth2LoginConfig
+            .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(googleOAuth2UserService))
+            .successHandler(oAuth2SuccessHandler)
+            ;
+    }
+
+    /**
      * 요청 URL 접근 설정
      */
     private void settingRequestAuthorization(HttpSecurity http) throws Exception {
@@ -73,9 +93,8 @@ public class WebSecurityConfig {
                 // 정적 파일
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 // 유저 도메인
-                .requestMatchers(HttpMethod.POST, "/users/signup").permitAll()
-                .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/users/logout").authenticated()
+                .requestMatchers("/login/oauth2/code/google").permitAll() // 구글 로그인 및 회원가입
+                .requestMatchers(HttpMethod.POST, "/users/logout").authenticated() // 로그아웃
                 // 그 외
                 .anyRequest().authenticated()
         );
