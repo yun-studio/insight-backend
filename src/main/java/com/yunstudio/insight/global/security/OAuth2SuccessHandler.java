43f54fcefb85f3ac78bdab6802e7bc060c43e5b1
barami62 <wodbs7758@naver.com>
2024-06-18 01:07:42 +0900

    Feat: OAuth2 로그인 성공 시 JWT 반환하도록 추가

--- src/main/java/com/yunstudio/insight/global/security/OAuth2SuccessHandler.java
+++ src/main/java/com/yunstudio/insight/global/security/OAuth2SuccessHandler.java
@@ -1,19 +1,26 @@
 package com.yunstudio.insight.global.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yunstudio.insight.domain.user.entity.User;
+import com.yunstudio.insight.global.jwt.JwtUtil;
 import com.yunstudio.insight.global.response.CommonResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
+    private final JwtUtil jwtUtil;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
@@ -23,10 +30,33 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         Authentication authentication
     ) throws IOException {
 
+        // 유저 파싱
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+        User user = principal.user();
+
+        // 응답 헤더에 JWT 추가
+        addTokensInHeader(response, user);
+
+        // 응답 바디에 성공 응답 객체 추가
+        settingResponseBody(response);
+    }
+
+    private void addTokensInHeader(HttpServletResponse response, User user) {
+        // JWT 생성
+        String accessToken = jwtUtil.createAccessToken(user.getNickname(), user.getRole().getAuthority());
+        String refreshToken = jwtUtil.createRefreshToken(user.getNickname(), user.getRole().getAuthority());
+
+        // 응답 헤더에 JWT 추가
+        response.addHeader(JwtUtil.ACCESS_TOKEN_HEADER, jwtUtil.setTokenWithBearer(accessToken)); // Access token
+        response.addHeader(JwtUtil.REFRESH_TOKEN_HEADER, jwtUtil.setTokenWithBearer(refreshToken)); // Refresh token
+    }
+
+    private void settingResponseBody(HttpServletResponse response) throws IOException {
+        // 바디 설정 추가
         response.setContentType(MediaType.APPLICATION_JSON_VALUE); // Content-Type : application/json
         response.setCharacterEncoding(StandardCharsets.UTF_8.name()); // charset : utf8
 
-        String json = objectMapper.writeValueAsString(CommonResponse.success()); // 빈 값을 가진 성공 응답
+        String json = objectMapper.writeValueAsString(CommonResponse.success());
         response.getWriter().write(json);
     }
 }
