ddbdbba269db9a3dd44e1e81d21fca4e06d9d40f
barami62 <wodbs7758@naver.com>
2024-06-18 23:36:23 +0900

    Feat: UserRepository에 닉네임으로 유저 찾는 메서드 추가

--- src/main/java/com/yunstudio/insight/domain/user/repository/UserRepository.java
+++ src/main/java/com/yunstudio/insight/domain/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByProviderId(String providerId);
+
+    Optional<User> findByNickname(String nickname);
 }
