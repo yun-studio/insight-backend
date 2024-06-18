0215f3b56d4a759492a16d491e8a73a67f834fdd
barami62 <wodbs7758@naver.com>
2024-06-18 12:39:28 +0900

    Fix: JWT 응답을 헤더에서 쿠키로 변경

--- src/main/java/com/yunstudio/insight/global/security/OAuth2SuccessHandler.java
+++ src/main/java/com/yunstudio/insight/global/security/OAuth2SuccessHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yunstudio.insight.domain.user.entity.User;
 import com.yunstudio.insight.global.jwt.JwtUtil;
 import com.yunstudio.insight.global.response.CommonResponse;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,6 +16,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+/**
+ * OAuth2 로그인 성공 시, 응답 쿠키에 JWT 추가
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -34,21 +38,35 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
         User user = principal.user();
 
-        // 응답 헤더에 JWT 추가
-        addTokensInHeader(response, user);
+        // 응답 쿠키에 JWT 추가
+        addTokensInCookie(response, user);
 
         // 응답 바디에 성공 응답 객체 추가
         settingResponseBody(response);
     }
 
-    private void addTokensInHeader(HttpServletResponse response, User user) {
+    private void addTokensInCookie(HttpServletResponse response, User user) {
         // JWT 생성
         String accessToken = jwtUtil.createAccessToken(user.getNickname(), user.getRole().getAuthority());
         String refreshToken = jwtUtil.createRefreshToken(user.getNickname(), user.getRole().getAuthority());
 
-        // 응답 헤더에 JWT 추가
-        response.addHeader(JwtUtil.ACCESS_TOKEN_HEADER, jwtUtil.setTokenWithBearer(accessToken)); // Access token
-        response.addHeader(JwtUtil.REFRESH_TOKEN_HEADER, jwtUtil.setTokenWithBearer(refreshToken)); // Refresh token
+        // JWT 담은 쿠키 생성
+        Cookie accessTokenCookie = createCookie(JwtUtil.ACCESS_TOKEN_HEADER, accessToken, JwtUtil.ACCESS_TOKEN_TTL_SECONDS);
+        Cookie refreshTokenCookie = createCookie(JwtUtil.REFRESH_TOKEN_HEADER, refreshToken, JwtUtil.REFRESH_TOKEN_TTL_SECONDS);
+
+        // 응답 쿠키에 JWT 추가
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+    }
+
+    private Cookie createCookie(String cookieName, String cookieValue, int maxAge) {
+        Cookie cookie = new Cookie(cookieName, cookieValue);
+
+        cookie.setMaxAge(maxAge);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+
+        return cookie;
     }
 
     private void settingResponseBody(HttpServletResponse response) throws IOException {
