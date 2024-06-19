6392bc2430a94428dbe897bb32cb77d738ba7b36
barami62 <wodbs7758@naver.com>
2024-06-19 16:02:33 +0900

    Fix: 리프레쉬 토큰 해시 삭제

--- src/main/java/com/yunstudio/insight/global/redis/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yunstudio.insight.global.redis;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-}
