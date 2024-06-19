0616a30fde0be482c226ab65bf0d83001f8cbd32
barami62 <wodbs7758@naver.com>
2024-06-19 16:02:12 +0900

    Fix: 리프레쉬 토큰 해시를 로그아웃 해시로 변경

--- src/main/java/com/yunstudio/insight/global/redis/RedisUtil.java
+++ src/main/java/com/yunstudio/insight/global/redis/RedisUtil.java
@@ -9,32 +9,23 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RedisUtil {
 
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final LogoutRepository logoutRepository;
 
-    /**
-     * Hash 타입으로 닉네임-리프레쉬 토큰 저장하여 로그인 처리
-     */
-    public void setUserLogin(String nickname, String refreshToken) {
-        RefreshToken refreshTokenItem = RefreshToken.builder()
+    public void setLogout(String nickname, int ttl) {
+        Logout logout = Logout.builder()
             .nickname(nickname)
-            .refreshToken(refreshToken)
+            .ttl(ttl)
             .build();
 
-        refreshTokenRepository.save(refreshTokenItem);
+        logoutRepository.save(logout);
     }
 
-    /**
-     * 레디스에 등록된 리프레쉬 토큰을 제거하여 로그아웃 처리
-     */
-    public void setUserLogout(String nickname) {
-        refreshTokenRepository.findById(nickname)
-            .ifPresent(refreshTokenRepository::delete);
+    public void setLogin(String nickname) {
+        logoutRepository.findById(nickname)
+            .ifPresent(logoutRepository::delete);
     }
 
-    /**
-     * 레디스에 등록된 리프레쉬 토큰 여부로 로그인 판단
-     */
-    public boolean isUserLogin(String nickname) {
-        return refreshTokenRepository.existsById(nickname);
+    public boolean isLogout(String nickname) {
+        return logoutRepository.existsById(nickname);
     }
 }
