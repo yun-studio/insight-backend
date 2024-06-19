147946c0b3c44947a9c33468c6acd7ba85c324f7
barami62 <wodbs7758@naver.com>
2024-06-19 19:51:29 +0900

    Feat: RefreshToken 레디스 해시 추가

---
+++ src/main/java/com/yunstudio/insight/global/redis/RefreshToken.java
@@ -0,0 +1,18 @@
+package com.yunstudio.insight.global.redis;
+
+import com.yunstudio.insight.global.jwt.JwtUtil;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@RedisHash(value = "refreshToken", timeToLive = JwtUtil.REFRESH_TOKEN_TTL_SECONDS)
+public class RefreshToken {
+
+    @Id
+    private String nickname;
+
+    private String refreshToken;
+}
