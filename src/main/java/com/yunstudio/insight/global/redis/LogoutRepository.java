2fbb881893d91d598119780dee3b6c2955674bc2
barami62 <wodbs7758@naver.com>
2024-06-19 19:53:18 +0900

    Remove: 필요없는 로그아웃 해시 삭제

--- src/main/java/com/yunstudio/insight/global/redis/LogoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yunstudio.insight.global.redis;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogoutRepository extends CrudRepository<Logout, String> {
-
-}
