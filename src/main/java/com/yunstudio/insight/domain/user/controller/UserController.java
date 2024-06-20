ef466d7d19ee6897369a29ddb4d6929b727db8d7
barami62 <wodbs7758@naver.com>
2024-06-20 16:57:34 +0900

    Feat: 닉네임 수정 API 추가

--- src/main/java/com/yunstudio/insight/domain/user/controller/UserController.java
+++ src/main/java/com/yunstudio/insight/domain/user/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.yunstudio.insight.domain.user.controller;
 
+import com.yunstudio.insight.domain.user.dto.request.UserUpdateNicknameReq;
 import com.yunstudio.insight.domain.user.dto.response.UserAnswerRes;
 import com.yunstudio.insight.domain.user.entity.User;
+import com.yunstudio.insight.domain.user.entity.UserRole;
 import com.yunstudio.insight.domain.user.service.UserService;
+import com.yunstudio.insight.global.jwt.JwtUtil;
+import com.yunstudio.insight.global.redis.RedisUtil;
 import com.yunstudio.insight.global.response.CommonResponse;
 import com.yunstudio.insight.global.security.LoginUser;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +42,17 @@ public class UserController {
         List<UserAnswerRes> userAnswerResList = userService.getLikeAnswers(user);
 
         return CommonResponse.success(userAnswerResList);
+    }
+
+    @PatchMapping("/nickname")
+    public CommonResponse<Object> changeNickname(
+        @LoginUser User user,
+        @RequestBody UserUpdateNicknameReq request,
+        HttpServletResponse response
+    ) {
+
+        userService.changeNickname(user, request.nickname(), response);
+
+        return CommonResponse.success();
     }
 }
