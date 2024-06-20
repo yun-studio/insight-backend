791c7d8c6a8a02ee46fe3d64c847009ac9d43e2f
barami62 <wodbs7758@naver.com>
2024-06-20 23:11:55 +0900

    Feat: 닉네임 변경 AOP 추가

--- src/main/java/com/yunstudio/insight/domain/user/aop/AuthAop.java
+++ src/main/java/com/yunstudio/insight/domain/user/aop/AuthAop.java
@@ -1,6 +1,8 @@
 package com.yunstudio.insight.domain.user.aop;
 
+import com.yunstudio.insight.domain.user.dto.response.UserChangeNicknameRes;
 import com.yunstudio.insight.domain.user.dto.response.UserDeleteRes;
+import com.yunstudio.insight.domain.user.entity.UserRole;
 import com.yunstudio.insight.global.jwt.JwtUtil;
 import com.yunstudio.insight.global.redis.RedisUtil;
 import com.yunstudio.insight.global.response.CommonResponse;
@@ -20,11 +22,66 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AuthAop {
 
+    private final JwtUtil jwtUtil;
     private final RedisUtil redisUtil;
     private final HttpServletResponse response;
 
+    @Pointcut("execution(* com.yunstudio.insight.domain.user.controller.UserController.changeNickname(..))")
+    private void forChangeNicknameMethod() {
+    }
+
     @Pointcut("execution(* com.yunstudio.insight.domain.user.controller.UserController.deleteUser(..))")
     private void forSoftDeleteUserMethod() {
+    }
+
+    @Around("forChangeNicknameMethod()")
+    public Object changeNickname(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 유저 닉네임 변경 로직 처리
+        Object result = joinPoint.proceed();
+
+        // 응답 객체 변환
+        CommonResponse<UserChangeNicknameRes> commonResponse = (CommonResponse<UserChangeNicknameRes>) result;
+        UserChangeNicknameRes userChangeNicknameRes = commonResponse.getData();
+
+        // 변경 전후 닉네임
+        String oldNickname = userChangeNicknameRes.oldNickname();
+        String newNickname = userChangeNicknameRes.newNickname();
+
+        log.info("변경 전 닉네임 : {}", oldNickname);
+        log.info("변경 후 닉네임 : {}", newNickname);
+
+        // 변경 전 닉네임 로그아웃 처리
+        redisUtil.setUserLogout(oldNickname);
+
+        // 액세스 토큰 처리
+        addAccessTokenInHeader(newNickname);
+
+        // 리프레쉬 토큰 처리
+        changeRefreshToken(newNickname);
+
+        return result;
+    }
+
+    private void addAccessTokenInHeader(String newNickname) {
+        // 변경 후 닉네임으로 액세스 토큰 발생
+        String accessToken = jwtUtil.setTokenWithBearer(jwtUtil.createAccessToken(newNickname, UserRole.USER.getAuthority()));
+
+        // 응답 객체에 추가
+        response.addHeader(JwtUtil.ACCESS_TOKEN_HEADER, accessToken);
+    }
+
+    private void changeRefreshToken(String newNickname) {
+        // 변경 후 닉네임으로 리프레쉬 토큰 발생
+        String refreshToken = jwtUtil.createRefreshToken(newNickname, UserRole.USER.getAuthority());
+
+        // 리프레쉬 토큰 쿠키 생성
+        Cookie refreshTokenCookie = jwtUtil.createRefreshTokenCookie(refreshToken);
+
+        // 응답 객체에 쿠키 추가
+        response.addCookie(refreshTokenCookie);
+
+        // 변경 후 닉네임으로 레디스 로그인 처리
+        redisUtil.setUserLogin(newNickname, refreshToken);
     }
 
     @Around("forSoftDeleteUserMethod()")
