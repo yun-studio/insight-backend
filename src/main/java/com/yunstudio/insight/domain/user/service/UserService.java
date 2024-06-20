7a18bd4bbc2bc782918150a4f2963b6242e756ca
barami62 <wodbs7758@naver.com>
2024-06-20 23:14:25 +0900

    Refactor: 닉네임 변경 유저 서비스 로직 변경

--- src/main/java/com/yunstudio/insight/domain/user/service/UserService.java
+++ src/main/java/com/yunstudio/insight/domain/user/service/UserService.java
@@ -2,13 +2,10 @@ package com.yunstudio.insight.domain.user.service;
 
 import com.yunstudio.insight.domain.answer.repository.AnswerRepository;
 import com.yunstudio.insight.domain.user.dto.response.UserAnswerRes;
+import com.yunstudio.insight.domain.user.dto.response.UserChangeNicknameRes;
 import com.yunstudio.insight.domain.user.dto.response.UserDeleteRes;
 import com.yunstudio.insight.domain.user.entity.User;
 import com.yunstudio.insight.domain.user.repository.UserRepository;
-import com.yunstudio.insight.global.jwt.JwtUtil;
-import com.yunstudio.insight.global.redis.RedisUtil;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,9 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
-    private final JwtUtil jwtUtil;
-    private final RedisUtil redisUtil;
 
     private final UserRepository userRepository;
     private final AnswerRepository answerRepository;
@@ -37,34 +31,16 @@ public class UserService {
     }
 
     @Transactional
-    public void changeNickname(User user, String newNickname, HttpServletResponse response) {
+    public UserChangeNicknameRes changeNickname(User user, String newNickname) {
 
         // 변경 전 닉네임
-        String prevNickname = user.getNickname();
-        log.info("변경 전 닉네임 : {}", prevNickname);
+        String oldNickname = user.getNickname();
 
         // 닉네임 변경
         user.changeNickname(newNickname);
-        User savedUser = userRepository.save(user);
+        userRepository.save(user);
 
-        log.info("변경 완료 닉네임 : {}", savedUser.getNickname());
-
-        // 변경 전 닉네임 로그아웃 처리
-        redisUtil.setUserLogout(prevNickname);
-
-        // 변경 후 닉네임으로 토큰 발생
-        String accessToken = jwtUtil.setTokenWithBearer(jwtUtil.createAccessToken(newNickname, user.getRole().getAuthority()));
-        String refreshToken = jwtUtil.createRefreshToken(newNickname, user.getRole().getAuthority());
-
-        // 리프레쉬 토큰 쿠키 생성
-        Cookie refreshTokenCookie = jwtUtil.createRefreshTokenCookie(refreshToken);
-
-        // 응답 객체에 추가
-        response.addHeader(JwtUtil.ACCESS_TOKEN_HEADER, accessToken);
-        response.addCookie(refreshTokenCookie);
-
-        // 변경 후 닉네임으로 로그인 처리
-        redisUtil.setUserLogin(newNickname, refreshToken);
+        return new UserChangeNicknameRes(oldNickname, newNickname);
     }
 
     @Transactional
