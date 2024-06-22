5e9094b047466471c140659cd7b0b457872c3ffe
barami62 <wodbs7758@naver.com>
2024-06-22 13:32:18 +0900

    Feat: 닉네임 변경 요청 DTO에 검증 어노테이션 추가

--- src/main/java/com/yunstudio/insight/domain/user/dto/request/UserUpdateNicknameReq.java
+++ src/main/java/com/yunstudio/insight/domain/user/dto/request/UserUpdateNicknameReq.java
@@ -1,6 +1,12 @@
 package com.yunstudio.insight.domain.user.dto.request;
 
+import jakarta.validation.constraints.Pattern;
+
 public record UserUpdateNicknameReq(
+    @Pattern(
+        regexp = "^[a-zA-Z0-9가-힣]{1,35}$", // 영대소문자, 한글 완성자 및 숫자가 1자 이상 35자 이하
+        message = "닉네임은 한글 및 숫자와 영어로 1자 이상 35자 이하만 가능합니다."
+    )
     String nickname
 ) {
 
