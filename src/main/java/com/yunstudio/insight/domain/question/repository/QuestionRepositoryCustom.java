d5537ead6ef2eec0843c2e1c68f97afbb3ab7afd
barami62 <wodbs7758@naver.com>
2024-06-24 18:09:47 +0900

    Refactor: 응답 DTO로 변환하여 응답하도록 QueryDsl 수정

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustom.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustom.java
@@ -1,9 +1,9 @@
 package com.yunstudio.insight.domain.question.repository;
 
-import com.yunstudio.insight.domain.question.entity.Question;
+import com.yunstudio.insight.domain.question.dto.response.GetQuestionRes;
 import java.util.List;
 
 public interface QuestionRepositoryCustom {
 
-    List<Question> findAllByQuery(String query);
+    List<GetQuestionRes> findAllByQuery(String query);
 }
--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustomImpl.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustomImpl.java
@@ -1,9 +1,14 @@
 package com.yunstudio.insight.domain.question.repository;
 
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
+import static com.yunstudio.insight.domain.answer.entity.QAnswer.answer;
 import static com.yunstudio.insight.domain.question.entity.QQuestion.question;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.yunstudio.insight.domain.question.entity.Question;
+import com.yunstudio.insight.domain.answer.dto.response.QGetAnswerRes;
+import com.yunstudio.insight.domain.question.dto.response.GetQuestionRes;
+import com.yunstudio.insight.domain.question.dto.response.QGetQuestionRes;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +18,26 @@ public class QuestionRepositoryCustomImpl implements QuestionRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Question> findAllByQuery(String query) {
-        return jpaQueryFactory.selectFrom(question)
+    public List<GetQuestionRes> findAllByQuery(String query) {
+
+        // 답변 응답 DTO
+        QGetAnswerRes qGetAnswerRes = new QGetAnswerRes(answer.id, answer.content, answer.createdAt, answer.modifiedAt);
+
+        // 질문 응답 DTO
+        QGetQuestionRes qGetQuestionRes = new QGetQuestionRes(
+            question.id,
+            question.content,
+            question.views,
+            list(qGetAnswerRes.skipNulls()),
+            question.createdAt,
+            question.modifiedAt
+        );
+
+        return jpaQueryFactory.select(qGetQuestionRes)
+            .from(question)
+            .leftJoin(answer)
+            .on(question.id.eq(answer.question.id))
             .where(question.content.containsIgnoreCase(query))
-            .fetch();
+            .transform(groupBy(question.id).list(qGetQuestionRes));
     }
 }
