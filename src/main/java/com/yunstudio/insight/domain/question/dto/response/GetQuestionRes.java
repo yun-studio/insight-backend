6aea052cb5f1aaa4909168bca98ea722977fe2d8
barami62 <wodbs7758@naver.com>
2024-06-24 18:08:00 +0900

    Feat: 질문 응답 DTO 추가

--- src/main/java/com/yunstudio/insight/domain/question/dto/response/GetQuestionRes.java
+++ src/main/java/com/yunstudio/insight/domain/question/dto/response/GetQuestionRes.java
@@ -1,13 +1,20 @@
 package com.yunstudio.insight.domain.question.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+import com.yunstudio.insight.domain.answer.dto.response.GetAnswerRes;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record GetQuestionRes(
     Long id,
     String content,
     Long views,
+    List<GetAnswerRes> answerList,
     LocalDateTime createdAt,
     LocalDateTime modifiedAt
 ) {
 
+    @QueryProjection // QueryDsl - DTO 변환 어노테이션
+    public GetQuestionRes {
+    }
 }
