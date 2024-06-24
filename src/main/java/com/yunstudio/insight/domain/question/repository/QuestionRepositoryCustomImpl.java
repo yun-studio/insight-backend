35d28c4b2e4e8f854b006f5bb1aa172e50c8feed
barami62 <wodbs7758@naver.com>
2024-06-24 23:05:26 +0900

    Feat: Pageable의 sort로 정렬이 되도록 수정

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustomImpl.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustomImpl.java
@@ -3,14 +3,17 @@ package com.yunstudio.insight.domain.question.repository;
 import static com.yunstudio.insight.domain.answer.entity.QAnswer.answer;
 import static com.yunstudio.insight.domain.question.entity.QQuestion.question;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.yunstudio.insight.domain.question.entity.Question;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Sort;
 
 @RequiredArgsConstructor
 public class QuestionRepositoryCustomImpl implements QuestionRepositoryCustom {
@@ -22,15 +25,32 @@ public class QuestionRepositoryCustomImpl implements QuestionRepositoryCustom {
 
         BooleanExpression predicate = question.content.upper().like("%" + content.toUpperCase() + "%");
 
-        List<Question> results = queryFactory
+        JPAQuery<Question> query = queryFactory
             .selectFrom(question)
             .leftJoin(question.answerList, answer).fetchJoin()
             .where(predicate)
-            .orderBy(question.answerList.size().asc())
             .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .fetch();
+            .limit(pageable.getPageSize());
 
-        return new SliceImpl<>(results);
+        // 정렬 조건 추가
+        for (Sort.Order order : pageable.getSort()) {
+            query.orderBy(createOrderSpecifier(order));
+        }
+
+        query.orderBy(question.createdAt.desc());
+
+        return new SliceImpl<>(query.fetch());
+    }
+
+    private OrderSpecifier createOrderSpecifier(Sort.Order order) {
+        String sortType = order.getProperty();
+        Order orderDirection = order.isAscending() ? Order.ASC : Order.DESC;
+
+        return switch (sortType) {
+            case "createdAt" -> new OrderSpecifier<>(orderDirection, question.createdAt);
+            case "answerCount" -> new OrderSpecifier<>(orderDirection, question.answerList.size());
+            case "views" -> new OrderSpecifier<>(orderDirection, question.views);
+            default -> new OrderSpecifier<>(Order.DESC, question.createdAt);
+        };
     }
 }
