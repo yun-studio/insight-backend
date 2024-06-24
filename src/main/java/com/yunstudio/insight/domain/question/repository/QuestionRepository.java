8edde254fb0a823af58b0827d6ce9535cf6fcb08
barami62 <wodbs7758@naver.com>
2024-06-24 19:11:22 +0900

    Feat: 쿼리 메서드로 목록 조회 메서드 추가

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepository.java
@@ -1,12 +1,15 @@
 package com.yunstudio.insight.domain.question.repository;
 
 import com.yunstudio.insight.domain.question.entity.Question;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     @Query(value = "select q from Question q join fetch q.answerList a where q.id = :id")
     Optional<Question> findById(Long id);
+
+    List<Question> findAllByContentContainingIgnoreCase(String content);
 }
