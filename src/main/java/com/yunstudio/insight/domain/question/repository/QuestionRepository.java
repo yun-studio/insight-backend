f831bdab0b6cbcbe6af8bc3dced9301b603482ca
barami62 <wodbs7758@naver.com>
2024-06-24 19:03:50 +0900

    Feat: 단건 조회 리포지토리 메서드 추가

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepository.java
@@ -1,10 +1,12 @@
 package com.yunstudio.insight.domain.question.repository;
 
 import com.yunstudio.insight.domain.question.entity.Question;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
 
-    List<Question> findAllByContentContainingIgnoreCase(String content);
+    @Query(value = "select q from Question q join fetch q.answerList a where q.id = :id")
+    Optional<Question> findById(Long id);
 }
