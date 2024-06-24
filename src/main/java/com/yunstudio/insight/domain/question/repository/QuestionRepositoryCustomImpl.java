d5c4bfcbab4e06279337d51e8e81118751719fb0
barami62 <wodbs7758@naver.com>
2024-06-24 19:02:20 +0900

    Fix: 목록 조회용 DTO로 QueryDsl 인터페이스 구현

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustomImpl.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustomImpl.java
@@ -1,14 +1,10 @@
 package com.yunstudio.insight.domain.question.repository;
 
-import static com.querydsl.core.group.GroupBy.groupBy;
-import static com.querydsl.core.group.GroupBy.list;
-import static com.yunstudio.insight.domain.answer.entity.QAnswer.answer;
 import static com.yunstudio.insight.domain.question.entity.QQuestion.question;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.yunstudio.insight.domain.answer.dto.response.QGetAnswerRes;
-import com.yunstudio.insight.domain.question.dto.response.GetQuestionRes;
-import com.yunstudio.insight.domain.question.dto.response.QGetQuestionRes;
+import com.yunstudio.insight.domain.question.dto.response.GetQuestionsRes;
+import com.yunstudio.insight.domain.question.dto.response.QGetQuestionsRes;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
@@ -18,26 +14,20 @@ public class QuestionRepositoryCustomImpl implements QuestionRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<GetQuestionRes> findAllByQuery(String query) {
-
-        // 답변 응답 DTO
-        QGetAnswerRes qGetAnswerRes = new QGetAnswerRes(answer.id, answer.content, answer.createdAt, answer.modifiedAt);
+    public List<GetQuestionsRes> findAllByQuery(String query) {
 
         // 질문 응답 DTO
-        QGetQuestionRes qGetQuestionRes = new QGetQuestionRes(
+        QGetQuestionsRes qGetQuestionsRes = new QGetQuestionsRes(
             question.id,
             question.content,
             question.views,
-            list(qGetAnswerRes.skipNulls()),
             question.createdAt,
             question.modifiedAt
         );
 
-        return jpaQueryFactory.select(qGetQuestionRes)
+        return jpaQueryFactory.select(qGetQuestionsRes)
             .from(question)
-            .leftJoin(answer)
-            .on(question.id.eq(answer.question.id))
             .where(question.content.containsIgnoreCase(query))
-            .transform(groupBy(question.id).list(qGetQuestionRes));
+            .fetch();
     }
 }
