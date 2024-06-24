a6bd4100a0317120869919f024cae63c313f3721
barami62 <wodbs7758@naver.com>
2024-06-24 19:12:17 +0900

    Remove: 필요 없는 QueryDsl 코드 삭제

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.yunstudio.insight.domain.question.repository;
-
-import com.yunstudio.insight.domain.question.dto.response.GetQuestionsRes;
-import java.util.List;
-
-public interface QuestionRepositoryCustom {
-
-    List<GetQuestionsRes> findAllByQuery(String query);
-}
