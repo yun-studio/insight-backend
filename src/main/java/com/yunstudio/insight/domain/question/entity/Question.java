042c775e889b9e1b35f0a5d4a41f694515adb331
barami62 <wodbs7758@naver.com>
2024-06-24 18:04:25 +0900

    Feat: 질문 엔티티에 답변 엔티티에 대한 일대다 필드 추가

--- src/main/java/com/yunstudio/insight/domain/question/entity/Question.java
+++ src/main/java/com/yunstudio/insight/domain/question/entity/Question.java
@@ -1,12 +1,16 @@
 package com.yunstudio.insight.domain.question.entity;
 
+import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,6 +34,9 @@ public class Question extends BaseEntity {
 
     @Column(name = "creator_id")
     private Long creatorId; // userId가 들어가지만, 연관관계는 없음.
+
+    @OneToMany(mappedBy = "question")
+    private List<Answer> answerList = new ArrayList<>();
 
     @Builder
     private Question(String content, Long creatorId) {
