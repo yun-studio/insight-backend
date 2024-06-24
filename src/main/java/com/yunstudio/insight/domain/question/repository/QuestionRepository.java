cde54aa69e363ec57a3a020efed79c4d6a8f1f2e
barami62 <wodbs7758@naver.com>
2024-06-24 19:25:20 +0900

    Refactor: 비관적 락으로 조회수 충돌 방지

--- src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/yunstudio/insight/domain/question/repository/QuestionRepository.java
@@ -1,15 +1,18 @@
 package com.yunstudio.insight.domain.question.repository;
 
 import com.yunstudio.insight.domain.question.entity.Question;
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(value = "select q from Question q join fetch q.answerList a where q.id = :id")
-    Optional<Question> findById(Long id);
+    Optional<Question> findByIdWithPessimisticLock(Long id);
 
     List<Question> findAllByContentContainingIgnoreCase(String content);
 }
