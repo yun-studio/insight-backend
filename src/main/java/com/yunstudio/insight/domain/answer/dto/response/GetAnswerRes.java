69783cd74a14a98e043876541241385f896a0cd6
barami62 <wodbs7758@naver.com>
2024-06-25 14:17:50 +0900

    Feat: 답변 응답 DTO에 좋아요 수 필드 추가

--- src/main/java/com/yunstudio/insight/domain/answer/dto/response/GetAnswerRes.java
+++ src/main/java/com/yunstudio/insight/domain/answer/dto/response/GetAnswerRes.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 public record GetAnswerRes(
     Long id,
     String content,
+    Integer likeCount,
     LocalDateTime createdAt,
     LocalDateTime modifiedAt
 ) {
