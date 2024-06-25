bb3b6b8a287591f9c01a4f32e923cc8e306c3c78
barami62 <wodbs7758@naver.com>
2024-06-25 14:19:30 +0900

    Feat: 질문 응답 DTO mapper에 답변 응답 DTO 매핑 로직 추가

--- src/main/java/com/yunstudio/insight/domain/question/mapper/QuestionMapper.java
+++ src/main/java/com/yunstudio/insight/domain/question/mapper/QuestionMapper.java
@@ -1,8 +1,11 @@
 package com.yunstudio.insight.domain.question.mapper;
 
+import com.yunstudio.insight.domain.answer.dto.response.GetAnswerRes;
+import com.yunstudio.insight.domain.answer.mapper.AnswerMapper;
 import com.yunstudio.insight.domain.question.dto.response.GetQuestionRes;
 import com.yunstudio.insight.domain.question.dto.response.GetQuestionsRes;
 import com.yunstudio.insight.domain.question.entity.Question;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -12,7 +15,20 @@ public interface QuestionMapper {
 
     QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
 
-    GetQuestionRes toGetQuestionRes(Question question);
+    default GetQuestionRes toGetQuestionRes(Question question) {
+        List<GetAnswerRes> getAnswerResList = question.getAnswerList()
+            .stream()
+            .map(AnswerMapper.INSTANCE::toGetAnswerRes)
+            .toList();
+
+        return new GetQuestionRes(
+            question.getId(),
+            question.getContent(),
+            question.getViews(),
+            getAnswerResList,
+            question.getCreatedAt(),
+            question.getModifiedAt());
+    }
 
     @Mapping(expression = "java(question.getAnswerList().size())", target = "answerCount")
     GetQuestionsRes toGetQuestionsRes(Question question);
