382ddaea1f09d6ced1b115f547a6c3aad5621eee
barami62 <wodbs7758@naver.com>
2024-06-26 14:21:52 +0900

    Refactor: 좋아요 리포지토리에 복합키 적용

--- src/main/java/com/yunstudio/insight/domain/like/repository/LikeRepository.java
+++ src/main/java/com/yunstudio/insight/domain/like/repository/LikeRepository.java
@@ -1,14 +1,9 @@
 package com.yunstudio.insight.domain.like.repository;
 
-import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.like.entity.Like;
-import com.yunstudio.insight.domain.user.entity.User;
-import java.util.Optional;
+import com.yunstudio.insight.domain.like.entity.LikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, LikeId> {
 
-    boolean existsByUserAndAnswer(User user, Answer answer);
-
-    Optional<Like> findByUserAndAnswer(User user, Answer answer);
 }
