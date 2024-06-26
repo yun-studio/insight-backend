11297b4df96d7cb5c627915830f16c25a4cce9a2
barami62 <wodbs7758@naver.com>
2024-06-26 12:00:48 +0900

    Feat: 서비스에 좋아요 추가 로직 추가

---
+++ src/main/java/com/yunstudio/insight/domain/like/service/LikeService.java
@@ -0,0 +1,50 @@
+package com.yunstudio.insight.domain.like.service;
+
+import com.yunstudio.insight.domain.answer.entity.Answer;
+import com.yunstudio.insight.domain.answer.repository.AnswerRepository;
+import com.yunstudio.insight.domain.like.entity.Like;
+import com.yunstudio.insight.domain.like.repository.LikeRepository;
+import com.yunstudio.insight.domain.user.entity.User;
+import com.yunstudio.insight.global.exception.GlobalException;
+import com.yunstudio.insight.global.response.CommonEmptyRes;
+import com.yunstudio.insight.global.response.ResultCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+    private final AnswerRepository answerRepository;
+
+    /**
+     * 좋아요 추가
+     */
+    @Transactional
+    public CommonEmptyRes likeUp(User user, Long answerId) {
+
+        Answer answer = answerRepository.findById(answerId)
+            .orElseThrow(() -> new GlobalException(ResultCase.ANSWER_NOT_FOUND));
+
+        if (isUserSameAuthor(user, answer)) {
+            throw new GlobalException(ResultCase.SELF_LIKE_NOT_ALLOWED);
+        }
+
+        Like like = Like.builder()
+            .user(user)
+            .answer(answer)
+            .build();
+
+        likeRepository.save(like);
+
+        return new CommonEmptyRes();
+    }
+
+    private boolean isUserSameAuthor(User user, Answer answer) {
+        return answer.getAuthor().getId().equals(user.getId());
+    }
+}
