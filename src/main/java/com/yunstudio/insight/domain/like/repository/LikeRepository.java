a88c6302d80505e542a4936e80163b9d2b129bb6
barami62 <wodbs7758@naver.com>
2024-06-26 12:50:11 +0900

    Feat: 유저와 답변으로 좋아요 찾는 메서드 추가

--- src/main/java/com/yunstudio/insight/domain/like/repository/LikeRepository.java
+++ src/main/java/com/yunstudio/insight/domain/like/repository/LikeRepository.java
@@ -3,9 +3,12 @@ package com.yunstudio.insight.domain.like.repository;
 import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.like.entity.Like;
 import com.yunstudio.insight.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     boolean existsByUserAndAnswer(User user, Answer answer);
+
+    Optional<Like> findByUserAndAnswer(User user, Answer answer);
 }
