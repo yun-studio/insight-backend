f2d6d1e28c53e7a3bc2f2edc47c440f9f9fc4b2e
barami62 <wodbs7758@naver.com>
2024-06-26 12:37:40 +0900

    Feat: 이미 좋아요 누른 여부 체크 메서드 추가

--- src/main/java/com/yunstudio/insight/domain/like/repository/LikeRepository.java
+++ src/main/java/com/yunstudio/insight/domain/like/repository/LikeRepository.java
@@ -1,8 +1,11 @@
 package com.yunstudio.insight.domain.like.repository;
 
+import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.like.entity.Like;
+import com.yunstudio.insight.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    
+
+    boolean existsByUserAndAnswer(User user, Answer answer);
 }
