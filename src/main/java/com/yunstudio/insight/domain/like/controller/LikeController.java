1dc7b613d322b3d05b46c6a0bd1910a38c21b694
barami62 <wodbs7758@naver.com>
2024-06-26 12:04:42 +0900

    Feat: 좋아요 추가 API 추가

---
+++ src/main/java/com/yunstudio/insight/domain/like/controller/LikeController.java
@@ -0,0 +1,33 @@
+package com.yunstudio.insight.domain.like.controller;
+
+import com.yunstudio.insight.domain.like.service.LikeService;
+import com.yunstudio.insight.domain.user.entity.User;
+import com.yunstudio.insight.global.response.CommonEmptyRes;
+import com.yunstudio.insight.global.response.CommonResponse;
+import com.yunstudio.insight.global.security.LoginUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/answers/{answerId}/like")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    /**
+     * 답변 좋아요
+     */
+    @PostMapping
+    public CommonResponse<CommonEmptyRes> likeUp(@LoginUser User user, @PathVariable Long answerId) {
+
+        CommonEmptyRes response = likeService.likeUp(user, answerId);
+
+        return CommonResponse.success(response);
+    }
+}
