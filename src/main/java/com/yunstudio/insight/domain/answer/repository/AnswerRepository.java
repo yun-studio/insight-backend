1c1ad154ca105a4c24548bd27d4c55742dbe525f
barami62 <wodbs7758@naver.com>
2024-06-26 14:20:31 +0900

    Remove: 필요없는 리포지토리 메서드 삭제

--- src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepository.java
@@ -1,12 +1,8 @@
 package com.yunstudio.insight.domain.answer.repository;
 
 import com.yunstudio.insight.domain.answer.entity.Answer;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerRepositoryCustom {
 
-    @Query(value = "select a from Answer a join fetch a.author u where a.id = :id")
-    Optional<Answer> findByIdWithAuthor(Long id);
 }
