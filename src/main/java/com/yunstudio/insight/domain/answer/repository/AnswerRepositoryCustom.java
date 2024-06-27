5d8d61ac4b68974c6d977032271560ca99e41c49
barami62 <wodbs7758@naver.com>
2024-06-27 16:32:52 +0900

    Feat: 다양한 분류기준으로 답변 가져오는 메서드 추가

--- src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepositoryCustom.java
+++ src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepositoryCustom.java
@@ -1,12 +1,17 @@
 package com.yunstudio.insight.domain.answer.repository;
 
+import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.user.dto.response.UserAnswerRes;
 import com.yunstudio.insight.domain.user.entity.User;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface AnswerRepositoryCustom {
 
     List<UserAnswerRes> findAllUserLikeAnswers(User user);
 
     List<UserAnswerRes> findAllByAuthor(User author);
+
+    Slice<Answer> findAllByQuestionPaging(Long questionId, Pageable pageable);
 }
