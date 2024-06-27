eb03edcdbd1a85532f0ac8b3dbf0e10025adcc4f
barami62 <wodbs7758@naver.com>
2024-06-27 16:00:40 +0900

    Fix: mapper에서 질문 속 답변 리스트를 Slice 타입으로 수정

--- src/main/java/com/yunstudio/insight/domain/question/mapper/QuestionMapper.java
+++ src/main/java/com/yunstudio/insight/domain/question/mapper/QuestionMapper.java
@@ -1,31 +1,31 @@
 package com.yunstudio.insight.domain.question.mapper;
 
 import com.yunstudio.insight.domain.answer.dto.response.GetAnswerRes;
+import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.answer.mapper.AnswerMapper;
 import com.yunstudio.insight.domain.question.dto.response.GetQuestionRes;
 import com.yunstudio.insight.domain.question.dto.response.GetQuestionsRes;
 import com.yunstudio.insight.domain.question.entity.Question;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Slice;
 
 @Mapper
 public interface QuestionMapper {
 
     QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
 
-    default GetQuestionRes toGetQuestionRes(Question question) {
-        List<GetAnswerRes> getAnswerResList = question.getAnswerList()
-            .stream()
-            .map(AnswerMapper.INSTANCE::toGetAnswerRes)
-            .toList();
+    default GetQuestionRes toGetQuestionRes(Question question, Slice<Answer> answerList) {
+        Slice<GetAnswerRes> getAnswerResList = answerList
+            .map(AnswerMapper.INSTANCE::toGetAnswerRes);
 
         return toGetQuestionResWithGetAnswerResList(question, getAnswerResList);
     }
 
+    @Mapping(source = "question.content", target = "content")
     @Mapping(source = "answerList", target = "answerList")
-    GetQuestionRes toGetQuestionResWithGetAnswerResList(Question question, List<GetAnswerRes> answerList);
+    GetQuestionRes toGetQuestionResWithGetAnswerResList(Question question, Slice<GetAnswerRes> answerList);
 
     @Mapping(expression = "java(question.getAnswerList().size())", target = "answerCount")
     GetQuestionsRes toGetQuestionsRes(Question question);
