9bd9cda77d0a0edc0790510a8400fe8fd69745b8
barami62 <wodbs7758@naver.com>
2024-06-27 16:32:58 +0900

    Feat: 다양한 분류기준으로 답변 가져오는 메서드 구현

--- src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepositoryCustomImpl.java
+++ src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepositoryCustomImpl.java
@@ -3,12 +3,20 @@ package com.yunstudio.insight.domain.answer.repository;
 import static com.yunstudio.insight.domain.answer.entity.QAnswer.answer;
 import static com.yunstudio.insight.domain.like.entity.QLike.like;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.yunstudio.insight.domain.answer.entity.Answer;
 import com.yunstudio.insight.domain.user.dto.response.UserAnswerRes;
 import com.yunstudio.insight.domain.user.entity.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Sort;
 
 @RequiredArgsConstructor
 public class AnswerRepositoryCustomImpl implements AnswerRepositoryCustom {
@@ -37,5 +45,34 @@ public class AnswerRepositoryCustomImpl implements AnswerRepositoryCustom {
             .on(answer.eq(like.answer))
             .where(answer.author.eq(author))
             .fetch();
+    }
+
+    @Override
+    public Slice<Answer> findAllByQuestionPaging(Long questionId, Pageable pageable) {
+
+        JPAQuery<Answer> query = jpaQueryFactory.selectFrom(answer)
+            .leftJoin(answer.likeList).fetchJoin()
+            .where(answer.question.id.eq(questionId))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize());
+
+        for (Sort.Order order : pageable.getSort()) {
+            query.orderBy(createOrderSpecifier(order));
+        }
+
+        query.orderBy(answer.createdAt.desc());
+
+        return new SliceImpl<>(query.fetch());
+    }
+
+    private OrderSpecifier<?> createOrderSpecifier(Sort.Order order) {
+        String sortType = order.getProperty();
+        Order orderDirection = order.isAscending() ? Order.ASC : Order.DESC;
+
+        return switch (sortType) {
+            case "createdAt" -> new OrderSpecifier<>(orderDirection, answer.createdAt);
+            case "likeCount" -> new OrderSpecifier<>(orderDirection, answer.likeList.size());
+            default -> new OrderSpecifier<>(Order.DESC, answer.createdAt);
+        };
     }
 }
