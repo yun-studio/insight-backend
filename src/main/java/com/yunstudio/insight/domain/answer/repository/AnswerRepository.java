c7b1dc4260c30198aa83957496e80f144dd5a3d3
barami62 <wodbs7758@naver.com>
2024-06-27 15:59:06 +0900

    Feat: 답변을 가져올 때 페이지네이션 적용

--- src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/yunstudio/insight/domain/answer/repository/AnswerRepository.java
@@ -1,8 +1,12 @@
 package com.yunstudio.insight.domain.answer.repository;
 
 import com.yunstudio.insight.domain.answer.entity.Answer;
+import com.yunstudio.insight.domain.question.entity.Question;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerRepositoryCustom {
 
+    Slice<Answer> findByQuestion(Question question, Pageable pageable);
 }
