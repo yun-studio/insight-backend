bdd5510ee1a4e9cf3f5f70f14b212413d7ca7605
barami62 <wodbs7758@naver.com>
2024-07-29 20:42:28 +0900

    Feat: 닉네임 변경 AOP 어노테이션 추가

---
+++ src/main/java/com/yunstudio/insight/domain/user/aop/annotation/ChangeRefreshTokenInRedis.java
@@ -0,0 +1,12 @@
+package com.yunstudio.insight.domain.user.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface ChangeRefreshTokenInRedis {
+
+}
